c9b8fb47d1aae4e47ecbfde0dfa2450a08b9f11a
Don Vince <don@donvince.co.uk>
2016-11-14 11:26:00 +0000

    First test - no assertions yet, just passes instantiating the empty class

---
+++ BowlingGameTest/BowlingGameTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            var g = new Game();
+        }
+    }
+}
