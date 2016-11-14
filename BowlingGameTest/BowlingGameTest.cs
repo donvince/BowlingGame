2fceffa102cb8527bca251567e5d305012136ab6
Don Vince <don@donvince.co.uk>
2016-11-14 11:47:05 +0000

    Extract method on the roll loop to RollMany()

--- BowlingGameTest/BowlingGameTest.cs
+++ BowlingGameTest/BowlingGameTest.cs
@@ -22,11 +22,18 @@ namespace BowlingGameTest
         [TestMethod]
         public void TestGutterGame()
         {
-            for (int i = 0; i < 20; i++)
-            {
-                g.Roll(0);
-            }
+            var n = 20;
+            int pins = 0;
+            RollMany(n, pins);
             Assert.AreEqual(0, g.Score());
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                g.Roll(pins);
+            }
         }
 
         [TestMethod]
